8dec399bb7d7b332664a5fae3670a20242bc706d
Django Doucet <mediaformat.ux@gmail.com>
2022-12-14 21:11:53 -0700

    update uninstall to target all fediembedi options

--- uninstall.php
+++ uninstall.php
@@ -3,26 +3,8 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
     die;
 }
 
-delete_option( 'fediembedi-notice' );
-
-//original options < 0.8.0
-delete_option( 'fediembedi-client-id' );
-delete_option( 'fediembedi-client-secret' );
-delete_option( 'fediembedi-token' );
-delete_option( 'fediembedi-instance' );
-delete_option( 'fediembedi-instance-info' );
-delete_option( 'fediembedi-instance-type' );
-
-//pixelfed
-delete_option('fediembedi-pixelfed-client-id');
-delete_option('fediembedi-pixelfed-client-secret');
-delete_option('fediembedi-pixelfed-token');
-delete_option('fediembedi-pixelfed-instance');
-delete_option('fediembedi-pixelfed-token');
-
-//mastodon
-delete_option('fediembedi-mastodon-client-id');
-delete_option('fediembedi-mastodon-client-secret');
-delete_option('fediembedi-mastodon-token');
-delete_option('fediembedi-mastodon-instance');
-delete_option('fediembedi-mastodon-token');
+foreach ( wp_load_alloptions() as $option => $value ) {
+    if ( strpos( $option, 'fediembedi-' ) === 0 ) {
+        delete_option( $option );
+    }
+}
